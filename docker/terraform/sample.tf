7cdbe18565a17be0e795ac32092550f78917ef97
Pooja Gangasagar <poojagangasagar@email.com>
2026-04-08 23:10:59 +0530

    first commit

---
+++ docker/terraform/sample.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "myinstance"{
+ami = ami-0ec10929233384c7f
+instance_type = "t3.micro"
+key_name ="id_rsa"
+vpc_security_group_ids = ["sg-053d5a946de99ca5b",]
+tags ={
+name = pooja
+Name = "myinstance"
+enviorment = "dev"
+}
+}
